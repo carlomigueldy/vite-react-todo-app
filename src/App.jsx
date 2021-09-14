ebe068a2f898427f2817713e871ca6f1a1abbeab
carlomigueldy <carlomigueldy@gmail.com>
2021-09-14 21:36:01 +0800

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,121 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+
+import { DebounceInput } from "react-debounce-input";
+
+/**
+ * 1. ✅ See todo listing
+ * 2. ✅ Add todo
+ * 3. ✅ Mark a todo as completed
+ * 4. ✅ Mark a todo as not completed
+ * 5. ✅ Delete a todo
+ * 6. ✅ Update a todo
+ */
+
+/**
+ *
+ * {
+ *    title: string
+ *    completed: boolean
+ * }
+ *
+ */
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formTodo, setFormTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    console.log(`Initial todos value: `, todos);
+
+    setTodos([
+      { title: "Storya", completed: false },
+      { title: "Ginhawa x2", completed: true },
+      { title: "Dula ug dulaan", completed: false },
+    ]);
+  }, []);
+
+  function addTodo(title) {
+    console.log(`title "${title}"`);
+
+    setTodos([...todos, { title: title, completed: false }]);
+  }
+
+  function markDone(todo) {
+    setTodos(
+      todos.map((item) =>
+        item.title === todo.title ? { ...item, completed: true } : item
+      )
+    );
+  }
+
+  function markNotDone(todo) {
+    const newTodos = todos.map((item) =>
+      item.title === todo.title ? { ...item, completed: false } : item
+    );
+
+    setTodos(newTodos);
+  }
+
+  function deleteTodo(todo) {
+    console.log("todos from useState", todos);
+
+    const newTodos = todos.filter((item) => item.title !== todo.title);
+
+    console.log("newTodos", newTodos);
+
+    setTodos(newTodos);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div
+      style={{
+        padding: 50,
+      }}
+    >
+      <h1>Hello World</h1>
+
+      <h3>Todos</h3>
+
+      <div>{JSON.stringify(todos)}</div>
+
+      <br />
+
+      <input
+        type="text"
+        placeholder="Add todo"
+        onChange={(event) => {
+          /** Handle on change event */
+          setFormTodo(event.target.value);
+
+          console.log("Print Me I am Handler");
+        }}
+      />
+      <button onClick={() => addTodo(formTodo)}>Create</button>
+
+      <ul>
+        {todos.map((todo, index) => {
+          // return <li key={index}>✅ {todo}</li>;
+          return (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={(event) => {
+                  if (todo.completed) {
+                    return markNotDone(todo);
+                  }
+
+                  markDone(todo);
+                }}
+              />
+              <label>{JSON.stringify(todo)}</label>
+              <button onClick={() => deleteTodo(todo)}>Delete Todo</button>
+            </div>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
